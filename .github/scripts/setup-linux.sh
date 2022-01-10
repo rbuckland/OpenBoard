8524f4a3c75fd7c79d54d47a9b496f64511b445e
Ramon Buckland <ramon@thebuckland.com>
2022-01-10 23:15:10 +1100

    update

--- .github/scripts/setup-linux.sh
+++ .github/scripts/setup-linux.sh
@@ -10,7 +10,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #
 # Setup build tooling
 #
-sudo apt install qmake
+# sudo apt install qmake
 
 #
 # Download 3rd party Libraries
@@ -18,16 +18,28 @@ sudo apt install qmake
 
 mkdir /build && cd /build
 
+# Ref: https://freetype.org/download.html
 curl -L -O https://download.savannah.gnu.org/releases/freetype/freetype-${FREETYPE_VERSION}.tar.gz \
 tar xvfz freetype-${FREETYPE_VERSION}.tar.gz
 
+# Ref: https://sourceforge.net/projects/quazip/files/quazip/
+#      http://quazip.sourceforge.net/
 curl -L -O https://sourceforge.net/projects/quazip/files/quazip/${QUAZIP_VERSION}/quazip-${QUAZIP_VERSION}.tar.gz/download \
 tar xvfz quazip-${QUAZIP_VERSION}.tar.gz
 
+# Ref: https://www.xpdfreader.com/download.html
 curl -L -O https://dl.xpdfreader.com/XpdfReader-linux64-4.03.run
 sh ./XpdfReader-linux64-${XPDF_VERSION}.run
 
-ls -al 
+# Ref: https://wiki.qt.io/Building_Qt_5_from_Git#Getting_the_source_code
+git clone https://code.qt.io/qt/qt5.git \
+  && cd qt5 \
+  && git checkout ${QT_VERSION}  \
+  && cd ..
+  
+mkdir qt5-build
+cd qt5-build
+../qt5/configure -developer-build -opensource -nomake examples -nomake tests
 
 #
 #
