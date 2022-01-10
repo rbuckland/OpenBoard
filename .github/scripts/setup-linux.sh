125dbb3e73e76529fa7f3db5e68990e18e168fa8
Ramon Buckland <ramon@thebuckland.com>
2022-01-10 22:54:43 +1100

    Mon 10 Jan 2022 22:54:43 AEDT

---
+++ .github/scripts/setup-linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+
+
+set -euxo pipefail
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+. ${DIR}/VERSIONS
+
+#
+# Setup build tooling
+#
+sudo apt install qmake
+
+#
+# Download 3rd party Libraries
+#
+
+mkdir /build && cd /build
+
+curl -L -O https://download.savannah.gnu.org/releases/freetype/freetype-${FREETYPE_VERSION}.tar.gz \
+tar xvfz freetype-${FREETYPE_VERSION}.tar.gz
+
+curl -L -O https://sourceforge.net/projects/quazip/files/quazip/${QUAZIP_VERSION}/quazip-${QUAZIP_VERSION}.tar.gz/download \
+tar xvfz quazip-${QUAZIP_VERSION}.tar.gz
+
+curl -L -O https://dl.xpdfreader.com/XpdfReader-linux64-4.03.run
+sh ./XpdfReader-linux64-${XPDF_VERSION}.run
+
+ls -al 
+
+#
+#
+#
